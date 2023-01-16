2ec6979485dd03a68fd5f40aa6d306e7309cfe2c
danii2020 <danielmauricioerazoespinoza@gmail.com>
2023-01-15 21:47:44 -0500

    Form added to Sign in page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { TextInput, View } from "react-native";
-import { Formik } from "formik";
+import { StyleSheet, View, Button } from "react-native";
+import { Formik, useField } from "formik";
+import StyledTextInput from "../StyledTextInput";
 
 const initialValues = {
   email: '',
   password: ''
+}
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 12
+  }
+})
+
+const FormikInputValue = ({ name, ...props }) => {
+  const [field, meta, helpers] = useField(name)
+  return (
+    <StyledTextInput
+    value={field.value}
+    onChangeText={value => helpers.setValue(value)}
+    {...props}
+  />
+  )
 }
 
 export default function Login () {
@@ -13,11 +31,19 @@ export default function Login () {
     initialValues={initialValues}
     onSubmit={values => console.log(values)}
     >
-      {() => {
+      {({handleSubmit}) => {
         return (
-          <View>
-            <TextInput />
-            <TextInput />
+          <View style={styles.form}>
+            <FormikInputValue
+              name="email"
+              placeholder="Email"
+            />
+            <FormikInputValue
+              name="password"
+              placeholder="Password"
+              secureTextEntry
+            />
+            <Button onPress={handleSubmit} title="Login"/>
           </View>
         )
       }}
